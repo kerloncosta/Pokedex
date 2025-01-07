fb6a579dcef8446e9ca431bf6cf5ccba02a06afb
kerloncosta <kerloncosta6@gmail.com>
2025-01-06 21:18:25 -0300

    Script, funcionamento e ajuste de tamanhos

--- js/script.js
+++ js/script.js
@@ -3,26 +3,65 @@ const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImage = document.querySelector('.pokemon_image');
 
 const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
 
 const fetchpokemon = async (pokemon) => {
 
     const APIRespoonse = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon);
-    const data = await APIRespoonse.json();
-    return data;
+    if(APIRespoonse.status == 200){
+        const data = await APIRespoonse.json();
+        return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
 
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
     const data =  await fetchpokemon(pokemon)
 
-    if(data.id <= 649){
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    if(data){
+        pokemonImage.style.display = 'block';
+        if(data.id <= 649){
+            pokemonName.innerHTML = data.name;
+            pokemonNumber.innerHTML = data.id;
+            pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+            input.value = "";
+            searchPokemon = data.id;
+            }else{
+                pokemonName.innerHTML = 'unregistered';
+                pokemonNumber.innerHTML = data.id;
+                pokemonImage.src = 'https://s3.pokeos.com/forgotten-dex/pokemon/0-missingNo.png';
+                input.value = "";
+            }
     }else{
-        pokemonName.innerHTML = 'Sem registros';
+        pokemonName.innerHTML = 'Not Found';
         pokemonNumber.innerHTML = '???';
-        pokemonImage.src = 'https://s3.pokeos.com/forgotten-dex/pokemon/0-missingNo.png';
-    }
+        pokemonImage.style.display = 'none';
+        input.value = "";
+    }  
 }
-renderPokemon('1000')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+});
+
+buttonPrev.addEventListener('click', () => {
+    if(searchPokemon > 1){
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }     
+});
+
+buttonNext.addEventListener('click', () => {
+        searchPokemon += 1;
+        renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
